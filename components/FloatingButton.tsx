45b6a2f42ed6deb5f1f17e6a10ff4e70d4a38684
Jan Moudrý <97884823+janMoudry@users.noreply.github.com>
2025-05-31 16:18:35 +0200

    Updated FloatingButton.tsx

--- components/FloatingButton.tsx
+++ components/FloatingButton.tsx
@@ -36,6 +36,6 @@ const styles = StyleSheet.create({
     shadowColor: colors.shadow,
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
-    shadowRadius: 8,
+    shadowRadius: 30,
   },
 });
